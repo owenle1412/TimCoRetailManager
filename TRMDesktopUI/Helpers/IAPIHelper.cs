23c2e2d9e12f9e7635ad75bd885205f1e1ff9b86
admin <ngocle.acc1412@gmail.com>
2022-11-13 10:19:22 +0700

    Wire Up Login Form To API

---
+++ TRMDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using TRMDesktopUI.Models;
+
+namespace TRMDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        HttpClient apiClient { get; set; }
+
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
